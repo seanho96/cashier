d03684f103e3269e5f2c0a2284d3a49e7bb3c8a6
Sean <seanho96@yahoo.com>
2020-08-20 21:39:25 +0800

    Updates

---
+++ precache-manifest.d2c3ca14498a6d7025b30efff65f3ee7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb024cf216a43a9c81d25c5dfa56da2",
+    "url": "/cashier/index.html"
+  },
+  {
+    "revision": "44b716be922a518069d1",
+    "url": "/cashier/static/css/main.cef20ef0.chunk.css"
+  },
+  {
+    "revision": "fa746ff6033947f987f9",
+    "url": "/cashier/static/js/2.3068ad67.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/cashier/static/js/2.3068ad67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44b716be922a518069d1",
+    "url": "/cashier/static/js/main.90113d82.chunk.js"
+  },
+  {
+    "revision": "d57bc3af17e30b06e5ed",
+    "url": "/cashier/static/js/runtime-main.768bf222.js"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/cashier/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/cashier/static/media/shopping-cart-solid.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/cashier/static/media/times-solid.2c5552a1.svg"
+  }
+]);
